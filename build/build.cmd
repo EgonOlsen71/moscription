1317e4468221131710af91de57b43bc99d4951ee
EgonOlsen <info@jpct.net>
2023-05-04 11:00:01 +0200

    Added title screen

--- build/build.cmd
+++ build/build.cmd
@@ -1,7 +1,8 @@
 del moscription_AD.d64
 c1541 -format moscription,bq d64 moscription_AD.d64
 call compile.cmd
-call ..\build\c1541 ..\build\moscription_AD.d64 -write ++cardy-c.prg moscription,p
+call ..\build\c1541 ..\build\moscription_AD.d64 -write ++loader-c.prg moscription,p
+call ..\build\c1541 ..\build\moscription_AD.d64 -write ++cardy-c.prg game,p
 call ..\build\c1541 ..\build\moscription_AD.d64 -write copy.prg copy,p
 call ..\build\c1541 ..\build\moscription_AD.d64 -write ..\basic\chars chars,p
 cd ..\build
--- build/compile.cmd
+++ build/compile.cmd
@@ -1,2 +1,4 @@
 call mospeed ..\basic\cardy.bas -compactlevel=4 -printopt=true -compression=true -generatesrc=true
+call mospeed ..\basic\loader.bas
+call moscrunch ++loader.prg -addfiles=..\res\moscription.img
 call mosm ..\basic\copy.asm
